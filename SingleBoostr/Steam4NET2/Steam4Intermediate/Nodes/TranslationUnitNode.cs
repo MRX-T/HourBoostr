cc990dfdf1c019eeeeae485ffc4b9bca92bb4a45
Twix <40114067+MRX-T@users.noreply.github.com>
2023-06-20 17:44:06 +0200

    update file

---
+++ SingleBoostr/Steam4NET2/Steam4Intermediate/Nodes/TranslationUnitNode.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Xml;
+using Steam4Intermediate.NodeBehavior;
+
+namespace Steam4Intermediate.Nodes
+{
+    class TranslationUnitNode : INode
+    {
+        public TranslationUnitNode(XmlAttributeCollection collection)
+            : base(collection)
+        {
+
+        }
+    }
+}
