a2efa31ed932cfa504da9860d98b3e14532f393e
Twix <40114067+MRX-T@users.noreply.github.com>
2023-06-20 17:44:04 +0200

    update file

---
+++ SingleBoostr/Steam4NET2/Steam4Intermediate/Nodes/ReferenceType.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Xml;
+using Steam4Intermediate.NodeBehavior;
+
+namespace Steam4Intermediate.Nodes
+{
+    class ReferenceTypeNode : LinkBehavior
+    {
+        public ReferenceTypeNode(XmlAttributeCollection collection)
+            : base(collection)
+        {
+
+        }
+    }
+}
