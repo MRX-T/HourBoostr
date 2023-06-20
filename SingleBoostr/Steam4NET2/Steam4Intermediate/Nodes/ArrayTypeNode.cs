b5bba09e962368a1a0ea526aded43d934a2bec5a
Twix <40114067+MRX-T@users.noreply.github.com>
2023-06-20 17:43:52 +0200

    update file

---
+++ SingleBoostr/Steam4NET2/Steam4Intermediate/Nodes/ArrayTypeNode.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Xml;
+using Steam4Intermediate.NodeBehavior;
+
+namespace Steam4Intermediate.Nodes
+{
+    class ArrayTypeNode : TypeLayerBehavior
+    {
+        public ArrayTypeNode(XmlAttributeCollection collection)
+            : base(collection)
+        {
+
+        }
+    }
+}
