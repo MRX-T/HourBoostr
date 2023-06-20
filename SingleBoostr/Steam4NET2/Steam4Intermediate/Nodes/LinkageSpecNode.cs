183f3248a83863e10787842dfaf9da2d7048e513
Twix <40114067+MRX-T@users.noreply.github.com>
2023-06-20 17:44:02 +0200

    update file

---
+++ SingleBoostr/Steam4NET2/Steam4Intermediate/Nodes/LinkageSpecNode.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Xml;
+using Steam4Intermediate.NodeBehavior;
+
+namespace Steam4Intermediate.Nodes
+{
+    class LinkageSpecNode : LinkBehavior
+    {
+        public LinkageSpecNode(XmlAttributeCollection collection)
+            : base(collection)
+        {
+
+        }
+    }
+}
